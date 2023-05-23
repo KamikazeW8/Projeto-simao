fa72f0f07bcfab163611c560a6bd3f3ae6f8e3a9
KamikazeW8 <131266894+KamikazeW8@users.noreply.github.com>
2023-05-23 09:18:36 -0300

    Update Principal.cpp

--- Principal.cpp
+++ Principal.cpp
@@ -1,7 +1,7 @@
 #include "Principal.hpp"
 
 Jungle::Principal::Principal():
-  window(sf::VideoMode(800.0f,600.0f), "Jungle++"),jogador()
+  pGrafico(pGrafico->getGerenciadorGrafico())
 {
   executar();
 }
@@ -15,23 +15,24 @@ void Jungle::Principal::executar(){
   sf::RectangleShape jogador(sf::Vector2f(200.0f,200.0f));
   jogador.setFillColor(sf::Color::Green);
 
- while (window.isOpen())
+ while (pGrafico->VerificaJanelaAberta())
  {
   sf::Event evento;
-  if(window.pollEvent(evento))
+  if(pGrafico->getWindow()->pollEvent(evento))
   if(evento.type == sf::Event::Closed)
-    window.close();
+    pGrafico->FecharJanela();
     else if (evento.type == sf::Event::KeyPressed)
     {
       if (evento.key.code == sf::Keyboard::Escape)
       {
-        window.close();
+        pGrafico->FecharJanela();
       }
     }
   }
-  window.clear();
+  pGrafico->limpaJanela();
   jogador.move();
   window.draw(jogador.getCorpo());
   window.display();
  }
+  
   
