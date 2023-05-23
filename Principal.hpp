9273bd559bbd43cfadedf58eb72daef3f1846f69
KamikazeW8 <131266894+KamikazeW8@users.noreply.github.com>
2023-05-23 09:18:53 -0300

    Update Principal.hpp

--- Principal.hpp
+++ Principal.hpp
@@ -1,12 +1,14 @@
-
+#pragma once
 
 #include "Jogador.hpp"
+#include "GerenciadorGrafico.hpp"
 
 namespace Jungle
 {
   class Principal
   {
   private:
+    Gerenciador::GerenciadorGrafico* pGrafico;
     sf::RenderWindow window;
     Jogador::Jogador jogador;
   public:
